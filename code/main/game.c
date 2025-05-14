8f14395b868cbc425a4d234c7c73ad9bdc0b8ad3
Oliver Dangl <oliver.dangl@st.oth-regensburg.de>
2025-05-15 01:45:26 +0200

    Added comment

--- code/main/game.c
+++ code/main/game.c
@@ -20,8 +20,8 @@ Function to reset a game by reseting player location and redrawing the maze
 void reset_game(GameState *game_state) {
     game_state->lives = MAX_LIVES;
     game_state->trap_visited = 0;
-    game_state->player.x = CELL_SIZE + 4;
-    game_state->player.y = CELL_SIZE + 4;
+    game_state->player.x = CELL_SIZE + 4; //small offset to wall
+    game_state->player.y = CELL_SIZE + 4; //small offset to wall
 
     for (int y = 0; y < MAZE_HEIGHT; y++) {
         for (int x = 0; x < MAZE_WIDTH; x++) {
